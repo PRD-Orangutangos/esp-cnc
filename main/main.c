2053af9445a36f9fc87e25c912ab92943ccf3ccf
aid219 <aid219@mail.ru>
2025-11-28 17:17:15 +0300

    test: test connection to wifi

--- main/main.c
+++ main/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "protocol_examples_common.h"
+#include <nvs_flash.h>
+#include "esp_event.h"
 
 void app_main(void)
 {
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 }
